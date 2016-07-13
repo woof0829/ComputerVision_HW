e97f6aa1b7de8254c186203b23202c5b5427b005
woof <woof0829@gmail.com>
2016-07-13 22:37:09 +0100

    fix the Class name in CamelCase

---
+++ assignment/Image_patch.py
@@ -0,0 +1,4 @@
+from Random_angle import GetAngle
+import math
+d = GetAngle()
+print(math.sin(d.get_angle()))
--- assignment/Random_angle.py
+++ assignment/Random_angle.py
@@ -1,13 +1,20 @@
 # Image patch generate
 import math
 import numpy as np
+import matplotlib as mpl
 
-class Get_angle(object):
-	"""Generate a random angle between 0 to pi/2"""
-	#def __init__(self, data):
-		#self.data=data
-	def get_angle(self):
-		angle = math.pi/2 * np.random.random((1,))
-		return float(angle)
-		pass
-		
+
+class GetAngle(object):
+    """Generate a random angle between 0 to pi/2"""
+
+    # def __init__(self, data):
+    # self.data=data
+    @staticmethod
+    def get_angle():
+        angle = math.pi / 2 * np.random.random((1,))
+        return float(angle)
+        pass
+
+    def function(self):
+        pass
+
