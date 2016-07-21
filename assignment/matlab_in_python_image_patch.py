aa9fd22dbbf001cb71a8c9fb9b1ecf1884e9cc99
woof <woof0829@gmail.com>
2016-07-21 14:42:19 +0100

    change row vector to column vector

--- assignment/matlab_in_python_image_patch.py
+++ assignment/matlab_in_python_image_patch.py
@@ -7,16 +7,20 @@ import matplotlib.pyplot as plt
 from Random_angle import GetAngle
 
 def imagepatchgenerate(ih, il, angle, location):
-    imagearray = 255 * np.random.uniform(0, 1, (401, 401))
-    imagearray[:, 201:401] = ih
-    imagearray[:, 0:200] = il
-    imagearray[:, 200] = (il+ih)/2
-    imagetemp = imagearray
-    standard_minus = np.std(imagetemp)
+    imagearray = 255 * np.random.uniform(0, 1, (11, 11))
+    imagearray[:, 6:11] = il
+    imagearray[:, 0:5] = ih
+    imagearray[:, 5] = (il+ih)/2
+    imagetemp = imagearray.flatten(1)
+    imagetemp.shape = (121, 1)
+    standard_minus = np.std(imagearray)
     d = GetAngle()
+    print imagetemp
     gaussianfilter=d.gaussian_2d((3, 3), standard_minus)
     c = scipy.ndimage.filters.correlate(gaussianfilter, imagetemp)
     b = scipy.misc.imrotate(gaussianfilter, angle, interp="bicubic")
-    e = plt.imshow(imagetemp)
-    plt.show(e)
-imagepatchgenerate(100, 1, 2*math.pi, 2)
+    e = plt.imshow(gaussianfilter)
+    plt.show(gaussianfilter)
+
+if __name__ == '__main__':
+    imagepatchgenerate(3, 1, 2*math.pi, 2)
