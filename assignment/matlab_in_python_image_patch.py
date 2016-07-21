16f47c3d6fb3afe4723e5e0417f7022e062c330c
woof <woof0829@gmail.com>
2016-07-21 14:51:50 +0100

    optimise the std function of numpy same as matlab

--- assignment/matlab_in_python_image_patch.py
+++ assignment/matlab_in_python_image_patch.py
@@ -13,14 +13,14 @@ def imagepatchgenerate(ih, il, angle, location):
     imagearray[:, 5] = (il+ih)/2
     imagetemp = imagearray.flatten(1)
     imagetemp.shape = (121, 1)
-    standard_minus = np.std(imagearray)
+    standard_minus = np.std(imagetemp, ddof=1)
     d = GetAngle()
-    print imagetemp
-    gaussianfilter=d.gaussian_2d((3, 3), standard_minus)
-    c = scipy.ndimage.filters.correlate(gaussianfilter, imagetemp)
-    b = scipy.misc.imrotate(gaussianfilter, angle, interp="bicubic")
-    e = plt.imshow(gaussianfilter)
-    plt.show(gaussianfilter)
+    print standard_minus
+    # gaussianfilter=d.gaussian_2d((3, 3), standard_minus)
+    # c = scipy.ndimage.filters.correlate(gaussianfilter, imagetemp)
+    # b = scipy.misc.imrotate(gaussianfilter, angle, interp="bicubic")
+    # e = plt.imshow(gaussianfilter)
+    # plt.show(gaussianfilter)
 
 if __name__ == '__main__':
     imagepatchgenerate(3, 1, 2*math.pi, 2)
