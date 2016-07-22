1b5416ec39fee525aad85931b18c6bf31a4dcb66
woof <woof0829@gmail.com>
2016-07-22 13:43:21 +0100

    add mlab library

--- assignment/matlab_in_python_image_patch.py
+++ assignment/matlab_in_python_image_patch.py
@@ -1,10 +1,11 @@
-import cv2
 import PIL
 import math
 import numpy as np
-import scipy.ndimage, scipy.misc
 import matplotlib.pyplot as plt
 from Random_angle import GetAngle
+from mlab.releases import latest_release as mtl
+
+
 
 def imagepatchgenerate(ih, il, angle, location):
     imagearray = 255 * np.random.uniform(0, 1, (11, 11))
@@ -15,12 +16,12 @@ def imagepatchgenerate(ih, il, angle, location):
     imagetemp.shape = (121, 1)
     standard_minus = np.std(imagetemp, ddof=1)
     d = GetAngle()
-    print standard_minus
-    # gaussianfilter=d.gaussian_2d((3, 3), standard_minus)
-    # c = scipy.ndimage.filters.correlate(gaussianfilter, imagetemp)
-    # b = scipy.misc.imrotate(gaussianfilter, angle, interp="bicubic")
-    # e = plt.imshow(gaussianfilter)
-    # plt.show(gaussianfilter)
+    gaussianfilter=d.gaussian_2d((3, 3), standard_minus)
+
+    e = plt.imshow(gaussianfilter[:,:], cmap=plt.cm.gray)
+    # mtl.imshow(gaussianfilter)
+    plt.show(e)
+    # print gaussianfilter
 
 if __name__ == '__main__':
     imagepatchgenerate(3, 1, 2*math.pi, 2)
