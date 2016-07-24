a4b02c897a5c3293545c1f0c97efa985713a510d
woof <woof0829@gmail.com>
2016-07-24 23:51:18 +0100

    add image crop function

--- assignment/matlab_in_python_image_patch.py
+++ assignment/matlab_in_python_image_patch.py
@@ -7,12 +7,12 @@ from mlab.releases import latest_release as mtl
 
 
 def imagepatchgenerate(ih, il, angle, location):
-    imagearray = 255 * np.random.uniform(0, 1, (11, 11))
-    imagearray[:, 6:11] = il
-    imagearray[:, 0:5] = ih
-    imagearray[:, 5] = (il+ih)/2
+    imagearray = 255 * np.random.uniform(0, 1, (201, 201))
+    imagearray[:, 101:201] = il
+    imagearray[:, 0:100] = ih
+    imagearray[:, 100] = (il+ih)/2
     imagetemp = imagearray.flatten(1)
-    imagetemp.shape = (121, 1)
+    imagetemp.shape = (201*201, 1)
     standard_minus = np.std(imagetemp, ddof=1)
     d = GetAngle()
     gaussianfilter = d.gaussian_2d((3, 3), standard_minus)
@@ -20,11 +20,11 @@ def imagepatchgenerate(ih, il, angle, location):
     rotatedimage = mtl.imrotate(image, angle, 'bicubic', 'crop')
     localx = location*math.cos(angle)
     localy = location*math.sin(angle)
-    translatedimage = mtl.imtranslate(rotatedimage, [[localx, localy]])
-    print translatedimage
-    # mtl.imshow(translatedimage)
+    translatedimage = mtl.imtranslate(imagearray, [[localx, localy]])
+    croppedimage = translatedimage[:, :]
+    print croppedimage
+
     # e = plt.imshow(translatedimage, cmap=plt.cm.gray)
     # plt.show(e)
-    # mtl.imshow(imagearray)
 if __name__ == '__main__':
     imagepatchgenerate(3, 1, math.pi/3, 3)
