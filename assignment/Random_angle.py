48626f1c516b327398fa3df00179a75dddfaa90f
woof <woof0829@gmail.com>
2016-07-26 22:03:04 +0100

    finish ImageRelated Class

--- assignment/Random_angle.py
+++ assignment/Random_angle.py
@@ -31,7 +31,7 @@ class ImageRelated(object):
         return h
 
     @staticmethod
-    def emptymatrix(x):
+    def empty_matrix(x=0):
         """
         generate empty n*n matrix
 
@@ -40,7 +40,7 @@ class ImageRelated(object):
         return matrix
 
     @staticmethod
-    def exchangevalue():
+    def exchange_value():
         """
         determine the value for High and Low
 
@@ -56,7 +56,11 @@ class ImageRelated(object):
         return [High, Low]
 
     @staticmethod
-    def setvalue(length=0, width=0, angle=math.pi, location=0, H=0, L=0):
+    def set_value(length=0, width=0, angle=math.pi, location=0, H=0, L=0):
+        """
+        set value for image matrix 
+
+        """
         matrix = np.zeros((length, width))
         for i in range(length):
             for j in range(width):
@@ -68,17 +72,38 @@ class ImageRelated(object):
                     matrix[j, i] = L
 
         return matrix
+
     @staticmethod
-    def meanvalue(number=0):
+    def mean_value(image=np.zeros((5, 5)), number=4):
+        """
+        calculate the mean value of the image patch
+
+        """
         for i in range(number):
             for j in range(number):
-                
+                temp_image = image[number*(j-1)+1:number*j, number*(i-1)+1:number*i]
+                temp_meanvalue = temp_image.flatten(1)
+                temp_meanvalue.shape = (number*number, 1)
+                mean_value = np.mean(temp_meanvalue)
+                meanvalue = np.round(mean_value)
+                image[j, i] = meanvalue
                 pass
             pass
-
         pass
-    
+        return image
+
+    @staticmethod
+    def judge_pixel(thres=0, pixel=0, image=[10, 10], n=0):
+        """
+        set value for central pixel
+
+        """
+        if pixel > thres:
+            image[n/2, n/2] = 255
+        else:
+            image[n/2, n/2] = 0
+            pass
+        pass
 
 if __name__ == '__main__':
     a = ImageRelated()
-
